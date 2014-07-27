27238b543e2c580088a4b12c5e4ac2ea80ea0971
seema shariat <seemashariat@gmail.com>
2014-07-27 16:49:38 -0400

    rafactorization complete. what did i learn... i learned that you cannot use case unless you are checking a case that is specific or a range of cases using the elispses. either that or i will learn what i actually did wrong in the case of using case and when some day soon. for now, i am fine using if and elsif because at least i am a lot faster at creating a simple program than i was 8 months ago.

--- horoscope_chinese.rb
+++ horoscope_chinese.rb
@@ -1,77 +1,28 @@
-# def animalcalc(year)
-#   if year == 1912 or year == 1924 or year == 1936 or year == 1948  or year == 1960 or year == 1972 or year == 1984 or year == 1996 or year == 2008
-#     return " rat"
-#   elsif year == 1913 or year == 1925 or year == 1937 or year == 1949 or year == 1961 or year == 1973 or year ==  1985 or year ==  1997 or year == 2009
-#       return "n ox"
-#   elsif year ==  1914 or year == 1926 or year ==  1938 or year == 1950 or year == 1962 or year ==  1974 or year ==  1986 or year ==  1998 or year ==  2010 
-#       return " tiger"
-#   elsif year == 1915 or year == 1927 or year ==  1939 or year == 1951 or year == 1963 or year == 1975 or year ==  1987 or year ==  1999  or year ==  2011 
-#       return " rabbit"
-#   elsif year == 1916 or year == 1928 or year ==  1940 or year == 1952 or year == 1964 or year == 1976 or year ==  1988 or year == 2000 or year ==  2012
-#       return " dragon"
-#    elsif year == 1917 or year == 1929 or year ==  1941 or year == 1953 or year == 1965 or year == 1977 or year ==  1989 or year == 2001   or year ==  2013
-#       return " snake"
-#    elsif year == 1918 or year == 1930 or year ==  1942 or year == 1954 or year == 1966 or year == 1978 or year ==  1990 or year == 2002 or year ==  2014
-#       return " horse"
-#    elsif year == 1919 or year == 1931 or year ==  1943 or year == 1955 or year == 1967 or year == 1979 or year ==  1991 or year == 2003
-#        return " sheep"
-#    elsif year == 1920 or year == 1932 or year ==  1944 or year == 1956 or year == 1968 or year == 1980 or year ==  1992 or year == 2004
-#       return " monkey"
-#    elsif year ==  1921 or year == 1933 or year ==  1945 or year == 1957  or year == 1969 or year == 1981 or year ==  1993 or year == 2005
-#       return " rooster"
-#    elsif year == 1922 or year == 1934 or year ==  1946 or year == 1958 or year == 1970  or year == 1982 or year ==  1994 or year == 2006
-#       return " dog" 
-#    elsif year == 1923 or year == 1935 or year ==  1947 or year == 1959 or year == 1971 or year == 1983 or year ==  1995 or year == 2007 
-#       return " pig"   
-#       end            
-# end
-
-# i = "horoscope_chinese"
-# until i == "Q" or i == "q"
-#  # Print the menu
-#   puts "Please select 1 to find out your Chinese spiritual animal."
-#   puts "Please select Q to quit."
- 
-#   i = gets.chomp  # for menu
-#   if i == "1"
-#     puts "Enter your first name"
-#     name = gets.chomp 
-#     puts "Enter your birth year in the form of yyyy." 
-#     userinputyear = gets.chomp
-#     puts "#{name} is a#{animalcalc(Integer(userinputyear))}"		# hashtag brackets takes representations like calculations and turns them into strings(Integer(x)) is an operation that turns strings into numbers			  				
-#   elsif i == "Q" or i == "q"
-#     puts "Thanks and see you later!"
-#   else 
-#     puts "This command is invalid"
-#   end
-# end
-
 def astro_sign_generator(year)
-puts year
- 
+
      if (year.to_i-1912).modulo(12) == 0
        "Rat."
-      elsif (year.to_i)-(1913).modulo(12) == 0
+      elsif (year.to_i-1913).modulo(12) == 0
         "Ox."
-      elsif (year.to_i)-(1914).modulo(12) == 0
+      elsif (year.to_i-1914).modulo(12) == 0
        "Tiger."
-      elsif (year.to_i)-(1915).modulo(12) == 0
+      elsif (year.to_i-1915).modulo(12) == 0
        "Rabbit."
-      elsif (year.to_i)-(1916).modulo(12) == 0
+      elsif (year.to_i-1916).modulo(12) == 0
          "Dragon."
-      elsif  (year.to_i)-(1917).modulo(12) == 0
+      elsif  (year.to_i-1917).modulo(12) == 0
         "Snake."
-      elsif (year.to_i)-(1918).modulo(12) == 0
+      elsif (year.to_i-1918).modulo(12) == 0
         "Horse."
-      elsif (year.to_i)-(1919).modulo(12) == 0
+      elsif (year.to_i-1919).modulo(12) == 0
         "Sheep."
-      elsif(year.to_i)-(1920).modulo(12) == 0
+      elsif(year.to_i-1920).modulo(12) == 0
         "Monkey."
-      elsif (year.to_i)-(1921).modulo(12) == 0
+      elsif (year.to_i-1921).modulo(12) == 0
         "Rooster."
-      elsif (year.to_i)-(1922).modulo(12) == 0
+      elsif (year.to_i-1922).modulo(12) == 0
         "Dog."
-      elsif (year.to_i)-(1923).modulo(12) == 0
+      elsif (year.to_i-1923).modulo(12) == 0
         "Pig."
         else 
           "Please enter a valid birth year. No information is provided for the year you entered."
